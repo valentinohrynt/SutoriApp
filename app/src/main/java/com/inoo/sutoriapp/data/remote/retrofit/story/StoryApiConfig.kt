386fd38029000450c4831db216d4f2aba65a854f
valentinohrynt <hariyantovalentino@gmail.com>
2024-10-18 14:51:30 +0700

    "initial commit submission-akhir (no-testing) ketinggalan"

---
+++ app/src/main/java/com/inoo/sutoriapp/data/remote/retrofit/story/StoryApiConfig.kt
@@ -0,0 +1,34 @@
+package com.inoo.sutoriapp.data.remote.retrofit.story
+
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import com.inoo.sutoriapp.BuildConfig
+import okhttp3.Interceptor
+import retrofit2.converter.gson.GsonConverterFactory
+
+class StoryApiConfig {
+    companion object {
+        private const val BASE_URL: String = BuildConfig.BASE_URL
+        fun getApiService(token: String): StoryApiService {
+            val authInterceptor = Interceptor { chain ->
+                val newRequest = chain.request().newBuilder()
+                    .addHeader("Authorization", "Bearer $token")
+                    .build()
+                chain.proceed(newRequest)
+            }
+            val loggingInterceptor =
+                HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+            val client = OkHttpClient.Builder()
+                .addInterceptor(authInterceptor)
+                .addInterceptor(loggingInterceptor)
+                .build()
+            val retrofit = Retrofit.Builder()
+                .baseUrl(BASE_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .client(client)
+                .build()
+            return retrofit.create(StoryApiService::class.java)
+        }
+    }
+}
